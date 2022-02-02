7e62c2b095e91d20216090226f9217b3893e15cd
shirsendu.nandi@gmail.com <shirsendu.nandi@gmail.com>
2022-02-02 16:44:56 +0530

    commit

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,11 +5,13 @@ namespace GrpcService.Models
 {
     public partial class Product
     {
+
         public int ProductRowId { get; set; }
         public string ProductId { get; set; }
         public string ProductName { get; set; }
         public string CategoryName { get; set; }
         public string Manufacturer { get; set; }
         public int Price { get; set; }
+
     }
 }
